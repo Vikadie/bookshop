c1895ec52cf94270c8cb36dd85273b51abb81940
Vikadie <68245263+Vikadie@users.noreply.github.com>
2023-09-14 17:51:27 +0300

    .adding the dist folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: { manifest: true },
+    build: { manifest: false },
     plugins: [react()],
 });
